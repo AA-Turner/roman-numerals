64650c2345876c57a2428f5dcec9fdd0d09edfc6
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-02-18 02:37:14 +0000

    Require the annotations future import

--- python/tests/test_round_trip.py
+++ python/tests/test_round_trip.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from roman_numerals import (
     MAX,
     MIN,
--- python/tests/utils.py
+++ python/tests/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 _TEST_NUMERALS_UPPER = [
     'I',
     'II',
